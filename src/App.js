b2f09a83357df1428599b54f93a997a3664ee4bc
Jr-Spring <jr.spring55@gmail.com>
2021-10-14 23:20:48 -0500

    Added day, date, month etc.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 const api = {
   key: "ef33f4f63af42482ea1fc1f5b95b3314",
   base: "https://api.openweathermap.org/data/2.5/"
@@ -6,8 +7,35 @@ const api = {
 
 
 function App() {
+
+  const  [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search  = evt => {
+    if (evt.key === "Enter") {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(res => res.json())
+    .then(result => setWeather(result));
+    
+    }
+  }
+
+const dateBuilder = (d) => {
+  // let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  // let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  
+  
+  // let day = days[d.getDay()];
+  // let date = d.getDate();
+  // let month = months[d.getMonth()];
+  // let year = d.getFullYear();
+
+  // return '${day} ${date} ${month} ${year}'
+
+}
+
   return (
-    <div className="App">
+    <div className="App sunny">
       <main>  
         <div className="search-box">
           <input
@@ -15,11 +43,19 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">St.Louis, MO USA</div>
+          {/* <div className="date">{dateBuilder(new Date())}</div> */}
+          <div className="date">{new Date().toDateString()}</div>
+        </div>
+        
+        <div className= "weather-box">
+          <div className= "temp">75&deg;F</div>
+          <div className="weather">Sunny</div>
 
         </div>
-      
-      
-      
+
       </main>
     </div>
   );
